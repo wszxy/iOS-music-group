a264eef077308d92cc8e0e4652643e39894e9bac
pdd123 <1365211318@qq.com>
2017-04-22 20:17:15 +0800

    1th work

---
+++ pdd/helloworldXML/helloworldXML/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  helloworldXML
+//
+//  Created by pdd on 2017/4/20.
+//  Copyright © 2017年 BNR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
